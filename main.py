d91d7c90dbba0e2d578126b5f0a36d53b6a18d23
Katie Christensen <christensen.katie00@gmail.com>
2023-12-11 18:15:53 -0800

    Remove secrets

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import sys
+import json
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import Optional, Dict, List
-from fastapi import FastAPI
 from typing import Optional, Dict, List
 
 sys.path.append('./src/')
@@ -14,12 +13,24 @@ app = FastAPI()
 
 @app.post("/KG")
 async def query_knowledge_graph(json_query: KG.Query):
-    db = KG.db_connect("neo4j://34.171.95.111:7687","neo4j","GeneData")
+    username, password, connection_string = get_credentials()
+    db = KG.db_connect(connection_string,username,password)
     result = KG.Query_KG_all(json_query,db)
     return(result)
 
 @app.get("/KG_types")
 async def get_types():
-    db = KG.db_connect("neo4j://34.171.95.111:7687","neo4j","GeneData")
+    username, password, connection_string = get_credentials()
+    db = KG.db_connect(connection_string,username,password)
     result = KG_types.get_KG_types(db)
     return(result)
+
+def get_credentials():
+    with open('/Users/katiechristensen/Desktop/credentials.json', 'r') as f:
+        data = json.load(f)
+
+    username = data['username']
+    password = data['password']
+    connection_string = data['connection_string']
+
+    return username, password, connection_string
